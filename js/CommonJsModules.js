6446e74f3a5108cf8c9797ccade56a6c90ca3f05
Calum Bradley <calumbradley@icloud.com>
2024-09-06 10:21:17 +0100

    update

--- js/CommonJsModules.js
+++ js/CommonJsModules.js
@@ -2,9 +2,9 @@
 
 This file demonstrates various ways to use CommonJS imports and exports in Node.js.
 
-```js
-// 1. Basic Export and Import Example (math.js)
+## 1. Basic Export and Import Example (`math.js`)
 
+```js
 // Exporting a function and a variable
 function add(a, b) {
   return a + b;
@@ -16,19 +16,19 @@ module.exports = {
   add,
   PI,
 };
+```
 
-// ----------------------------------------
-// 2. Single Value Export (logger.js)
+## Exporting a function directly
 
-// Exporting a function directly
+```js
 module.exports = function log(message) {
   console.log(message);
 };
+```
 
-// ----------------------------------------
-// 3. Named Exports (utils.js)
+## Exporting multiple values (functions)
 
-// Exporting multiple values (functions)
+```js
 const formatDate = (date) => {
   return date.toISOString();
 };
@@ -41,20 +41,20 @@ module.exports = {
   formatDate,
   greet,
 };
+```
 
-// ----------------------------------------
-// 4. Aliasing Imports Example (in app.js)
+## Importing specific parts and renaming
 
-// Importing specific parts and renaming
+```js
 const { formatDate: format, greet: sayHello } = require('./utils');
 
 console.log(format(new Date()));      // Alias `format`
 console.log(sayHello('Alice'));       // Alias `sayHello`
+```
 
-// ----------------------------------------
-// 5. Class Export Example (Person.js)
+## Exporting a class
 
-// Exporting a class
+```js
 class Person {
   constructor(name) {
     this.name = name;
@@ -66,20 +66,19 @@ class Person {
 }
 
 module.exports = Person;
+```
 
-// ----------------------------------------
-// 6. Exporting a Default Object Example (config.js)
+## Exporting an object directly
 
-// Exporting an object directly
+```js
 module.exports = {
   port: 3000,
   db: 'mongodb://localhost/myapp',
 };
+```
 
-// ----------------------------------------
-// 7. Conditional Export Example (db.js)
-
-// Exporting a value based on the environment
+## Exporting a value based on the environment
+```js
 let dbConnection;
 
 if (process.env.NODE_ENV === 'production') {
@@ -89,27 +88,14 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 module.exports = dbConnection;
+```
 
-// ----------------------------------------
-// 8. Importing Built-in Modules Example (app.js)
-
-// Importing Node's built-in File System module
+## Importing Node's built-in File System module
+```js
 const fs = require('fs');
 
 fs.readFile('file.txt', 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data);
 });
-
-// ----------------------------------------
-// 9. Circular Dependencies Example (fileA.js and fileB.js)
-
-// fileA.js
-const fileB = require('./fileB');
-console.log('fileA loaded');
-module.exports = 'This is fileA';
-
-// fileB.js
-const fileA = require('./fileA');
-console.log('fileB loaded');
-module.exports = 'This is fileB';
+```
